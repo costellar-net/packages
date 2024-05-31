4ebe02ed72f14c44c15bf1dc1a5ece3cd10a3574
neo <nudes.zip@gmail.com>
2024-05-31 17:10:53 -0400

    [ui] todo: fix rollup

--- ui/src/components/Button/Button.tsx
+++ ui/src/components/Button/Button.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classes from './Button.module.css';
 
-const Button: React.FC = () => {
-	return <button>Button</button>;
+interface P extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+	icon?: React.ReactNode;
+	title: string;
+	disabled?: boolean;
+}
+
+const Button: React.FC<P> = ({ icon, className, title, ...rest }) => {
+	const [styling, setStyling] = useState([classes.button]);
+
+	useEffect(() => {
+		if (className) {
+			setStyling([...styling, ...className.split(' ')]);
+		}
+	}, []);
+
+	return (
+		<button aria-label={title} className={styling.join(' ')} {...rest}>
+			{icon && icon}
+			<p>{title}</p>
+		</button>
+	);
 };
 
 export default Button;
